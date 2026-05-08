1f5e48288562dd93e921be973532624de07d312b
Hrishikesh Tiwari <tiwarihrishikesh686@gmail.com>
2026-05-08 09:13:02 +0530

    get and post method done, this time using a python object db

---
+++ week2/Books.py
@@ -0,0 +1,48 @@
+from fastapi import FastAPI, Body  # type: ignore
+
+
+app = FastAPI()
+
+
+# Lets create a Book python object
+class Book: 
+    
+    id: int 
+    title : str 
+    author : str 
+    description: str 
+    rating : int 
+    
+    def __init__ (self, id, title, author, description, rating): 
+        self.id = id 
+        self.title = title 
+        self.author = author 
+        self.description = description
+        self.rating = rating
+    
+     
+BOOKS = [
+    Book(1, "Introduction to Graphs", "Hrishikesh", "CS Book" , 4.5), 
+    Book(2, "Faster FastAPI", "margot robbie", "great book", 5), 
+    Book(3, "Midjourney Crisis", "dakota jhonson", "brilliant novel", 4), 
+    Book(4, "Jane Austere Milley", "Ana D Armas", "gothic Novel", 5), 
+]
+
+@app.get("/")
+async def helloWorld(): 
+    return "Hello World!"
+
+# GET METHOD
+
+@app.get("/books")
+async def getAllBooks(): 
+    return BOOKS
+
+
+# POST METHOD 
+
+@app.post("/createBook")
+async def createBook(newBook = Body()): 
+    BOOKS.append(newBook)
+    
+    
